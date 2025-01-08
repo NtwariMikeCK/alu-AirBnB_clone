c7d59d6772f4c1a4de91d4f7e0f7d99c58498d20
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-08 09:46:27 +0200

    created a BaseModel for creating a unique id and the time it is created at

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+    
+    def __init__(self):
+        """Initialize a new BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """Return a dictionary representation of the instance."""
+        model_dict = self.__dict__.copy()
+        model_dict["__class__"] = self.__class__.__name__
+        model_dict["created_at"] = self.created_at.isoformat()
+        model_dict["updated_at"] = self.updated_at.isoformat()
+        return model_dict
