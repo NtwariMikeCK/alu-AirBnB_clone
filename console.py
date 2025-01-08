52d5df1d4332dcbdcc3305e666bf8fb4a226a9ef
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-08 15:39:15 +0200

    creating a basic CLI for functionality

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for managing AirBnB objects"""
+    prompt = "(hbnb) "  # Custom shell prompt
+
+    def do_quit(self, arg):
+        """Quit command to exit the interpreter"""
+        return True
+
+    def do_EOF(self, arg):
+        """Handles End Of File (Ctrl+D) to exit"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Does nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
