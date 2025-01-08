5f9b717ca92c36242ff129080c47eaf71467a6f4
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-08 11:31:43 +0200

    added a new test for checking if a new instance created is not equal to the old instance

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,10 +13,15 @@ class TestBaseModel(unittest.TestCase):
     def setUp(self):
         """Create a new instance before each test"""
         self.model = BaseModel()
+        self.model.name = "Test Model"
+        self.model.number = 42
 
     def test_id_type(self):
         """Test that the id is a string"""
         self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model, BaseModel)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_id_unique(self):
         """Test that each instance has a unique id"""
@@ -50,10 +55,28 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(model_dict["created_at"], created_at_str)
         self.assertEqual(model_dict["updated_at"], updated_at_str)
 
+    def test_to_dict_method(self):
+        """Test dictionary conversion"""
+        obj_dict = self.model.to_dict()
+        self.assertEqual(obj_dict["__class__"], "BaseModel")
+        self.assertEqual(obj_dict["name"], "Test Model")
+        self.assertEqual(obj_dict["number"], 42)
+        self.assertIsInstance(obj_dict["created_at"], str)
+        self.assertIsInstance(obj_dict["updated_at"], str)
+
     def test_str_representation(self):
         """Test the string representation of the instance"""
         expected_str = f"[BaseModel] ({self.model.id}) {self.model.__dict__}"
         self.assertEqual(str(self.model), expected_str)
+
+    def test_create_instance_from_dict(self):
+        """Test creating an instance from a dictionary"""
+        obj_dict = self.model.to_dict()
+        new_model = BaseModel(**obj_dict)
+        self.assertEqual(self.model.id, new_model.id)
+        self.assertEqual(self.model.created_at, new_model.created_at)
+        self.assertEqual(self.model.updated_at, new_model.updated_at)
+        self.assertFalse(self.model is new_model)
 
 
 if __name__ == '__main__':
