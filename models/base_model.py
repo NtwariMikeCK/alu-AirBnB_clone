46765c25b8c870045a04661e150f5d7100a154e6
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-08 14:25:31 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 import uuid
 from datetime import datetime
+# from models import storage
 
 class BaseModel:
     """BaseModel class that defines common attributes/methods"""
@@ -18,6 +19,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage  # Import here to avoid circular import
+            storage.new(self)  # Add new instance to storage
 
     def __str__(self):
         """String representation of the instance"""
@@ -26,6 +29,8 @@ class BaseModel:
     def save(self):
         """Updates `updated_at` with the current datetime"""
         self.updated_at = datetime.now()
+        from models import storage 
+        storage.save()  # Save to JSON file
 
     def to_dict(self):
         """Converts instance into a dictionary format"""
