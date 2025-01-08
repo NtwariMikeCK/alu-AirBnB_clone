c0bb97bd50afb94d80eecb71a24782d9c7a9d2ad
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-08 11:34:56 +0200

    Added a the **kwargs to handle instance creation when it is created manually

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes."""
-    
-    def __init__(self):
-        """Initialize a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
+    """BaseModel class that defines common attributes/methods"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
     def __str__(self):
-        """Return a string representation of the instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+        """String representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
-        """Update the updated_at attribute with the current datetime."""
+        """Updates `updated_at` with the current datetime"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
-        """Return a dictionary representation of the instance."""
-        model_dict = self.__dict__.copy()
-        model_dict["__class__"] = self.__class__.__name__
-        model_dict["created_at"] = self.created_at.isoformat()
-        model_dict["updated_at"] = self.updated_at.isoformat()
-        return model_dict
+        """Converts instance into a dictionary format"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
