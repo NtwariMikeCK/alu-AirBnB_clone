30c8765c16562b200783bc05ac327309e50774b9
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-16 16:08:10 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
+class Review(BaseModel):
     """ This is a class responsible for the state"""
     place_id = ""
     user_id = ""
